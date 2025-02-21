4e1dbdf71a7a99f95936700bb967bf8b21688b79
Christine Nyambwari <christinenyambwari@gmail.com>
2025-02-21 07:30:42 -0800

    commit

--- src/session/session.router.ts
+++ src/session/session.router.ts
@@ -6,11 +6,11 @@ import { adminRoleAuth } from '../middleware/bearAuth'
 import {therapistRoleAuth } from '../middleware/bearAuth'
 export const sessionRouter = new Hono();
 //get all session
-sessionRouter.get("/session",adminRoleAuth ,listsession) 
-
+sessionRouter.get("/session" ,listsession) 
+// ,adminRoleAuth
 //get a single therapist   api/therapist/1
-sessionRouter.get("/session/:id",adminRoleAuth, getsession)
-
+sessionRouter.get("/session/:id", getsession)
+// ,adminRoleAuth
 // create a therapist 
 sessionRouter.post("/session", zValidator('json', sessionSchema, (result, c) => {
     if (!result.success) {
